9e2d502dce5d7df1803b3b27a624c2989b4026fc
Kauan Borges <kauanbgs13@gmail.com>
2024-12-24 01:22:35 -0300

    Cash OK

--- cash-exercise.c
+++ cash-exercise.c
@@ -9,6 +9,22 @@ int main (void) {
   double cent3 = 0.10;
   double cent4 = 0.25;
    
-  double price = get_int("Price: ");
+  double change = get_float("Change: ");
+  int numCoins = 0;
+  int changeInCents = (int)round(change * 100);
+
+  numCoins += changeInCents / 25;
+  changeInCents %= 25;
+
+  numCoins += changeInCents / 10;
+  changeInCents %= 10;
+
+  numCoins += changeInCents / 5;
+  changeInCents %= 5;
+
+   numCoins += changeInCents / 1;
+  changeInCents %= 1;
+
+  printf("Coins used: %d \n", numCoins);
   
 }
