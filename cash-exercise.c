1f6eb6f85cf5ca2559131dfcbea21c20911b5f76
Kauan Borges <kauanbgs13@gmail.com>
2024-12-24 01:01:02 -0300

    starting cash

---
+++ cash-exercise.c
@@ -0,0 +1,14 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <math.h>
+
+int main (void) {
+
+  double cent1 = 0.01;
+  double cent2 = 0.05;
+  double cent3 = 0.10;
+  double cent4 = 0.25;
+   
+  double price = get_int("Price: ");
+  
+}
